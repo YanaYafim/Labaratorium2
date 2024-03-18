e252cfca7933056e6bea631c07f812891b59264f
YanaYafim <158202884+YanaYafim@users.noreply.github.com>
2024-03-18 21:04:56 +0100

    Zadania z lab2

---
+++ Zadanie1/app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const PORT = 3000
+
+
+app.use(express.static('views'))
+
+const server = app.listen(PORT, () => {
+	console.log('Server is running on ${PORT}')
+})
+
+const renderPage = (res, fileName) => {
+	res.sendFile('Zadanie1/views/${fileName}.html')
+}
+
+app.get('/', (req, res) => {
+	renderPage(res, 'home')
+})
+
+app.get('/student', (req, res) => {
+	renderPage(res, 'student')
+})
